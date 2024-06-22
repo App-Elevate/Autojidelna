8591d3250bca0606a7d948ed5d2ca50996bfac0c
tpkowastaken <protitom@gmail.com>
2024-06-23 00:46:29 +0200

    Revert "ran flutterfire configure"
    This reverts commit 94f487b28c1ccdbfe5883c255005fbdb50d0a6b9.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:core/src/ui/main.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:core/firebase_options.dart';
 
-void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
