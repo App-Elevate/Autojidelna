e0e2cd112afdc14245b6497bac44a5d1737239c1
tpkowastaken <protitom@gmail.com>
2024-06-22 15:23:06 +0200

    setup structure

---
+++ lib/src/ui/screens/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:core/src/ui/pages/my_app.dart';
+import 'package:flutter/material.dart';
+
+class MainAppScreen extends StatelessWidget {
+  const MainAppScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyHomePage(
+      title: 'Flutter Demo Home Page',
+      key: Key('k'),
+    );
+  }
+}
