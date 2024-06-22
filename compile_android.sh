94f487b28c1ccdbfe5883c255005fbdb50d0a6b9
tpkowastaken <protitom@gmail.com>
2024-06-23 00:17:13 +0200

    ran flutterfire configure

--- compile_android.sh
+++ compile_android.sh
@@ -1,2 +1,2 @@
 flutter build appbundle --split-debug-info symbolsAndroid --obfuscate
-#firebase crashlytics:symbols:upload --app=1:939051306678:android:01036b4a8805db6784e017 symbolsAndroid
+firebase crashlytics:symbols:upload --app=1:466976066624:android:804f4c712efa78d15423a5 symbolsAndroid
