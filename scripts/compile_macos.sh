3fd1e7003ea94f7894ea95e6e53c1e47fba0bac4
tpkowastaken <protitom@gmail.com>
2024-06-30 19:19:14 +0200

    try

--- scripts/compile_macos.sh
+++ scripts/compile_macos.sh
@@ -1,3 +1,3 @@
-cd macos && bundle install && bundle exec fastlane match appstore --readonly &&
+cd macos && bundle install && bundle exec fastlane match appstore --additional_cert_types=mac_installer_distribution --readonly &&
 cd .. && flutter build macos --split-debug-info symbolsMacOS --obfuscate &&
 cd macos && bundle exec fastlane release
