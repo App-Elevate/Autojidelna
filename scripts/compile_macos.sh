d156a1bc59b79e4b35e5f64b1521001f7a774859
tpkowastaken <protitom@gmail.com>
2024-07-01 15:31:34 +0200

    this is the best i can do

--- scripts/compile_macos.sh
+++ scripts/compile_macos.sh
@@ -1,3 +1,3 @@
-cd macos && bundle install && bundle exec fastlane match appstore --readonly -p temp &&
+cd macos && bundle install && bundle exec fastlane match appstore --readonly &&
 cd .. && flutter build macos --split-debug-info symbolsMacOS --obfuscate &&
 cd macos && bundle exec fastlane release
