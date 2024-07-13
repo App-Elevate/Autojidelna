31f79f0a2a4fbaa94922a4a71b655f30128cca59
tpkowastaken <protitom@gmail.com>
2024-07-14 01:28:41 +0200

    working permissions

--- lib/src/ui/pages/crashlytics_page.dart
+++ lib/src/ui/pages/crashlytics_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:coree/src/lang/lang.dart';
 import 'package:coree/src/crash_testing/crash.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -21,7 +22,7 @@ class CrashlyticsPage extends StatelessWidget {
           TextButton(onPressed: crashlyticsTestFunction, child: Text(Alocale.crashlyticsTestCrash.getString(context))),
           TextButton(
             onPressed: () async {
-              if (await Permission.locationWhenInUse.request() == PermissionStatus.granted) {
+              if (await Permission.location.request() == PermissionStatus.granted) {
                 debugPrint('Permission granted');
               } else {
                 debugPrint('Permission denied');
