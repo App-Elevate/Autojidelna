9c6ae44d5a1cc2f12ee8b9006e6bde37a9bb71b4
Matěj <matej@appelevate.cz>
2024-07-20 20:22:52 +0200

    adaptive layout test 1

--- lib/src/ui/pages/router_page.dart
+++ lib/src/ui/pages/router_page.dart
@@ -1,33 +1,43 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:coree/src/lang/l10n_context_extension.dart';
-import 'package:coree/src/_routing/app_router.gr.dart';
+import 'package:coree/src/ui/pages/crashlytics_page.dart';
+import 'package:coree/src/ui/pages/demo_page.dart';
+import 'package:coree/src/ui/pages/settings/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 
 @RoutePage()
-class RouterPage extends StatelessWidget {
+class RouterPage extends StatefulWidget {
   const RouterPage({super.key});
 
   @override
+  State<RouterPage> createState() => _RouterPageState();
+}
+
+class _RouterPageState extends State<RouterPage> {
+  int index = 0;
+  changeIndex(int newIndex) => setState(() => index = newIndex);
+  @override
   Widget build(BuildContext context) {
     final lang = context.l10n;
-    return AutoTabsScaffold(
-      // These should Always be constant.
-      routes: const [
-        DemoPage(),
-        CrashlyticsPage(),
-        SettingsPage(),
+    return AdaptiveScaffold(
+      useDrawer: false,
+      transitionDuration: Durations.short1,
+      selectedIndex: index,
+      bodyRatio: .4,
+      onSelectedIndexChange: changeIndex,
+      destinations: [
+        NavigationDestination(label: lang.counting, icon: const Icon(Icons.numbers)),
+        NavigationDestination(label: lang.crashlytics, icon: const Icon(Icons.bug_report)),
+        NavigationDestination(label: lang.settings, icon: const Icon(Icons.settings)),
       ],
-      bottomNavigationBuilder: (context, tabsRouter) {
-        return BottomNavigationBar(
-          currentIndex: tabsRouter.activeIndex,
-          onTap: tabsRouter.setActiveIndex,
-          items: [
-            BottomNavigationBarItem(label: lang.counting, icon: const Icon(Icons.numbers)),
-            BottomNavigationBarItem(label: lang.crashlytics, icon: const Icon(Icons.bug_report)),
-            BottomNavigationBarItem(label: lang.settings, icon: const Icon(Icons.settings)),
-          ],
-        );
-      },
+      body: (context) => [
+        const DemoPage(),
+        const CrashlyticsPage(),
+        const SettingsPage(),
+      ][index],
+      // Only ever use [largeSecondaryBody] in case more content is wanted on the screen
+      // largeSecondaryBody: (context) => Container(color: Colors.red),
     );
   }
 }
