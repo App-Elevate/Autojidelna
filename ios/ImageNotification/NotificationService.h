c863630c3b6fc51934e262c1256cc4d7a733c81a
tpkowastaken <protitom@gmail.com>
2024-07-22 22:33:17 +0200

    compiles

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by tom on 22.07.2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
