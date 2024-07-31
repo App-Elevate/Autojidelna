c4beb05daa5ce404683ac466dfbb173911646bef
tpkowastaken <protitom@gmail.com>
2024-07-31 15:29:31 +0200

    fixed a bug with messaging deeplinks

--- lib/src/_routing/app_router.dart
+++ lib/src/_routing/app_router.dart
@@ -8,14 +8,19 @@ import 'package:coree/src/_routing/guards.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Screen,Route')
 class AppRouter extends $AppRouter implements AutoRouteGuard {
+  bool isValid(AutoRoute route, String baseRoute, {String prevRoute = ''}) {
+    if (route.children != null && route.children!.routes.isNotEmpty) {
+      final match = route.children!.routes.any((child) => isValid(child, baseRoute, prevRoute: prevRoute + route.path));
+      if (match) return true;
+    }
+    final regexPattern = RegExp('^${(prevRoute + route.path).replaceAllMapped(RegExp(r':\w+'), (match) => r'[^/]+')}\$');
+    return regexPattern.hasMatch(baseRoute);
+  }
+
   bool isRouteValid(String routeName) {
     final uri = Uri.parse(routeName);
-    final baseRoute = uri.path;
-    return routes.any((route) {
-      // Convert the route pattern to a regex pattern
-      final regexPattern = RegExp('^${route.path.replaceAllMapped(RegExp(r':\w+'), (match) => r'[^/]+')}\$');
-      return regexPattern.hasMatch(baseRoute);
-    });
+    String baseRoute = uri.path;
+    return routes.any((route) => isValid(route, baseRoute));
   }
 
   @override
@@ -27,7 +32,10 @@ class AppRouter extends $AppRouter implements AutoRouteGuard {
       final uri = Uri.parse(routeName);
       final route = uri.path + (uri.hasQuery ? '?${uri.query}' : '');
       resolver.next();
-      unawaited(router.pushNamed(route));
+      Future.delayed(
+        Duration.zero,
+        () => unawaited(router.root.navigateNamed(route)),
+      );
       return;
     }
     resolver.next();
@@ -39,8 +47,8 @@ class AppRouter extends $AppRouter implements AutoRouteGuard {
         AutoRoute(
           page: RouterPage.page,
           initial: true,
-          path: '/',
           guards: [NotificationGuard()],
+          path: '/',
           children: <AutoRoute>[
             AutoRoute(page: DemoPage.page, path: 'demo'),
             AutoRoute(page: CrashlyticsPage.page, path: '', initial: true),
