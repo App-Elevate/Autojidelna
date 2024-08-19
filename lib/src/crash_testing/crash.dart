ad35f245ec2219cc8ca76efa62821dd78deddb4c
tpkowastaken <protitom@gmail.com>
2024-08-20 01:11:47 +0200

    sentry web test

--- lib/src/crash_testing/crash.dart
+++ lib/src/crash_testing/crash.dart
@@ -1,4 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
 /// This is a test function that will crash the app.
-void crashlyticsTestFunction() {
+void crashlyticsTestFunction() async {
+  if (kIsWeb) {
+    try {
+      throw StateError('HEY SENTRY WEB');
+    } catch (exception, stackTrace) {
+      await Sentry.captureException(
+        exception,
+        stackTrace: stackTrace,
+      );
+    }
+    return;
+  }
   throw StateError('HEY SENTRY');
 }
