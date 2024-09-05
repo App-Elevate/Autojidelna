282b6a61d611889d1da2a12427724fefc00aac98
tpkowastaken <protitom@gmail.com>
2024-09-05 12:26:04 +0200

    patch

--- lib/src/lang/output/texts.dart
+++ lib/src/lang/output/texts.dart
@@ -164,7 +164,7 @@ abstract class Texts {
   /// No description provided for @crashlyticsPage.
   ///
   /// In en, this message translates to:
-  /// **'Crashlytics Page'**
+  /// **'Crashlytics Page Patched'**
   String get crashlyticsPage;
 
   /// No description provided for @crashlyticsTestCrash.
