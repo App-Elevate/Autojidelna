ffbfe3da1bc71be561549e24d0a03da2992b2dd0
tpkowastaken <protitom@gmail.com>
2024-09-05 14:00:33 +0200

    patch works

--- lib/src/lang/output/texts.dart
+++ lib/src/lang/output/texts.dart
@@ -164,7 +164,7 @@ abstract class Texts {
   /// No description provided for @crashlyticsPage.
   ///
   /// In en, this message translates to:
-  /// **'Crashlytics Page Patched'**
+  /// **'Crashlytics Page Patched again'**
   String get crashlyticsPage;
 
   /// No description provided for @crashlyticsTestCrash.
--- lib/src/lang/output/texts_en.dart
+++ lib/src/lang/output/texts_en.dart
@@ -47,7 +47,7 @@ class TextsEn extends Texts {
   String get crashlytics => 'Crashlytics';
 
   @override
-  String get crashlyticsPage => 'Crashlytics Page Patched';
+  String get crashlyticsPage => 'Crashlytics Page Patched again';
 
   @override
   String get crashlyticsTestCrash => 'Crashlytics test crash!';
