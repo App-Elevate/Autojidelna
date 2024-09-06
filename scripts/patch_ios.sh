3ca449702fb30125fca0832860d213daa6be4ecf
tpkowastaken <protitom@gmail.com>
2024-09-06 12:56:59 +0200

    fixed soft error

--- scripts/patch_ios.sh
+++ scripts/patch_ios.sh
@@ -19,7 +19,7 @@ bundle install
 bundle exec fastlane match appstore --readonly 
 cd .. 
 
-yes | shorebird patch ios --release-version=$version -- --split-debug-info="build/symbolsIos"
+yes | shorebird patch ios --release-version=$version --export-options-plist=/Users/builder/export_options.plist -- --split-debug-info="build/symbolsIos"
 
 cd ios 
 bundle exec fastlane release_ci
