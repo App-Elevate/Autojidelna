f898f087bac6e1714aeb5270e1ddb3b406ecd234
Matěj Verhaegen <matej@appelevate.cz>
2024-10-05 11:47:53 +0200

    change getter to const in SecureStorage()

--- lib/src/_conf/secure_storage.dart
+++ lib/src/_conf/secure_storage.dart
@@ -2,7 +2,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 /// Secure storage keys for FlutterSecureStorage and options for iOS
 class SecureStorage {
-  static String get loginData => 'loginData';
-  static String get appleAuthKey => 'apple_auth_key';
-  static get options => const IOSOptions(accessibility: KeychainAccessibility.first_unlock);
+  static const String loginData = 'loginData';
+  static const String appleAuthKey = 'apple_auth_key';
+  static const IOSOptions iosOptions = IOSOptions(accessibility: KeychainAccessibility.first_unlock);
 }
