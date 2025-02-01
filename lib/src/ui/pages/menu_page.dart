3c612093b9a7a9c6df872a1ded18235bf358fb04
Matěj Verhaegen <matej@appelevate.cz>
2025-02-01 23:45:08 +0100

    update MenuPage()

--- lib/src/ui/pages/menu_page.dart
+++ lib/src/ui/pages/menu_page.dart
@@ -1,47 +1,19 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:autojidelna/src/_global/app.dart';
-import 'package:autojidelna/src/_global/providers/dishes_of_the_day_provider.dart';
-import 'package:autojidelna/src/logic/datetime_wrapper.dart';
-import 'package:autojidelna/src/ui/widgets/canteen/page_view/menu_of_the_day.dart';
+import 'package:autojidelna/src/_global/providers/settings.provider.dart';
+import 'package:autojidelna/src/ui/widgets/canteen/list_view/list_view_canteen.dart';
+import 'package:autojidelna/src/ui/widgets/canteen/page_view/page_view_canteen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 @RoutePage()
-class MenuPage extends StatefulWidget {
+class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
 
   @override
-  State<MenuPage> createState() => _MenuPageState();
-}
-
-class _MenuPageState extends State<MenuPage> {
-  @override
-  void initState() {
-    super.initState();
-    App.pageController = PageController(
-      keepPage: true,
-      initialPage: convertDateTimeToIndex(DateTime.now()),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    App.pageController.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: () async {
-        //TODO: Implement refresh
-      },
-      child: PageView.builder(
-        controller: App.pageController,
-        onPageChanged: context.read<DishesOfTheDay>().setDayIndex,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (_, index) => MenuOfTheDay(index),
-      ),
+    return Selector<Settings, bool>(
+      selector: (_, p1) => p1.isListUi,
+      builder: (context, data, child) => data ? const ListViewCanteen() : const PageViewCanteen(),
     );
   }
 }
---
+++ lib/src/ui/widgets/canteen/page_view/page_view_canteen.dart
@@ -0,0 +1,45 @@
+import 'package:autojidelna/src/_global/app.dart';
+import 'package:autojidelna/src/_global/providers/dishes_of_the_day_provider.dart';
+import 'package:autojidelna/src/logic/datetime_wrapper.dart';
+import 'package:autojidelna/src/ui/widgets/canteen/page_view/menu_of_the_day.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PageViewCanteen extends StatefulWidget {
+  const PageViewCanteen({super.key});
+
+  @override
+  State<PageViewCanteen> createState() => _PageViewCanteenState();
+}
+
+class _PageViewCanteenState extends State<PageViewCanteen> {
+  @override
+  void initState() {
+    super.initState();
+    App.pageController = PageController(
+      keepPage: true,
+      initialPage: convertDateTimeToIndex(DateTime.now()),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    App.pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: () async {
+        //TODO: Implement refresh
+      },
+      child: PageView.builder(
+        controller: App.pageController,
+        onPageChanged: context.read<DishesOfTheDay>().setDayIndex,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, index) => MenuOfTheDay(index),
+      ),
+    );
+  }
+}
