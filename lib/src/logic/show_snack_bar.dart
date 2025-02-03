2365080d8d82e0e6ecccdee42d01e3d666ac4583
Matěj Verhaegen <matej@appelevate.cz>
2025-02-03 11:35:59 +0100

    add loginSuccessSnackBar()

--- lib/src/logic/show_snack_bar.dart
+++ lib/src/logic/show_snack_bar.dart
@@ -4,6 +4,7 @@ import 'package:autojidelna/src/_global/app.dart';
 import 'package:autojidelna/src/_routing/app_router.dart';
 import 'package:autojidelna/src/ui/widgets/snackbars/error_snack_bar.dart';
 import 'package:autojidelna/src/ui/widgets/snackbars/info_snack_bar.dart';
+import 'package:autojidelna/src/ui/widgets/snackbars/login_success_snack_bar.dart';
 import 'package:flutter/material.dart';
 
 void _showSnackBar(SnackBar snackbar) {
@@ -24,5 +25,12 @@ void showInfoSnackBar(IconData icon, String title, String? subtitle) {
   BuildContext? ctx = App.getIt<AppRouter>().navigatorKey.currentContext;
   if (ctx == null) return;
 
-  _showSnackBar(infoSnackBar(ctx, title: title, subtitle: subtitle));
+  _showSnackBar(infoSnackBar(ctx, icon: icon, title: title, subtitle: subtitle));
+}
+
+void showLoginSuccessSnackBar(String username) {
+  BuildContext? ctx = App.getIt<AppRouter>().navigatorKey.currentContext;
+  if (ctx == null) return;
+
+  _showSnackBar(loginSuccessSnackBar(ctx, username));
 }
--- lib/src/ui/widgets/snackbars/info_snack_bar.dart
+++ lib/src/ui/widgets/snackbars/info_snack_bar.dart
@@ -1,11 +1,11 @@
 import 'package:autojidelna/src/ui/widgets/snackbars/base_snack_bar.dart';
 import 'package:flutter/material.dart';
 
-SnackBar infoSnackBar(BuildContext context, {String title = '', String? subtitle}) {
+SnackBar infoSnackBar(BuildContext context, {String title = '', String? subtitle, IconData icon = Icons.info_outline_rounded}) {
   return baseSnackBar(
     context,
     foregroundColor: Theme.of(context).colorScheme.onInverseSurface,
-    icon: Icons.info_outline_rounded,
+    icon: icon,
     title: title,
     subtitle: subtitle,
   );
