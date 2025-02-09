cc11758680f0ab20829d4a903e76776712c801c6
Matěj Verhaegen <matej@appelevate.cz>
2025-02-09 02:37:49 +0100

    create CanteenService()

---
+++ lib/src/logic/services/canteen_service.dart
@@ -0,0 +1,4 @@
+class CanteenService {
+  /// Sets how many max lunches are expected. The higher the worse performance but less missing lunches. This is a fix for the api sometimes not sending all the lunches
+  static const int numberOfLunches = 3;
+}
