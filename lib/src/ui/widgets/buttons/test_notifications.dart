7f52f9fe8959bb1f0988f3cd8c910771da0b6f73
Matěj Verhaegen <matej@appelevate.cz>
2025-02-19 10:27:20 +0100

    add const

--- lib/src/ui/widgets/buttons/test_notifications.dart
+++ lib/src/ui/widgets/buttons/test_notifications.dart
@@ -5,7 +5,7 @@ class NotificationActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
         ListTile(onTap: null /* TODO: () async => doNotifications(force: true), title: const Text('Notifications Force')*/),
         ListTile(onTap: null /* TODO: () async => doNotifications(), title: const Text('Notifications')*/),
