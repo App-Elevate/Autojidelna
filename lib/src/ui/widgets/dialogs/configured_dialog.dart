edf8a16353cb182d97ee7e086de58c8c2be33caf
Matěj Verhaegen <matej@appelevate.cz>
2025-02-20 19:43:41 +0100

    update configured alert dialog

--- lib/src/ui/widgets/dialogs/configured_dialog.dart
+++ lib/src/ui/widgets/dialogs/configured_dialog.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-void configuredDialog(BuildContext context, {required Widget Function(BuildContext) builder}) {
+void configuredDialog(BuildContext context, {required Widget Function(BuildContext context) builder}) {
   unawaited(
     showDialog(
       context: context,
